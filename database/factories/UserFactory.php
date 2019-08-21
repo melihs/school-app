6f3376d6e93be5951f2e591db48eabfab56dbfca
melihs <melihsahin24@gmail.com>
2019-08-22 00:10:21 +0300

    user tablosu düzenlendi ve seed oluşturuldu

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,14 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $status = ['student','family','students_family'];
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'status' => Arr::random($status),
+        'code' =>  Str::random(4),
         'remember_token' => Str::random(10),
     ];
 });
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('status',['student','family','students_family']);
+            $table->string('code');
             $table->rememberToken();
             $table->timestamps();
         });
