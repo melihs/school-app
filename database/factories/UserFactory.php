3f8a1723291822e29e1455ef1282ca7f2732b59e
melihs <melihsahin24@gmail.com>
2019-08-24 12:57:39 +0300

    status durumu 'student' olarak güncellendi

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,13 +18,12 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
-    $status = ['student','family'];
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => Hash::make(1234), // password
-        'status' => Arr::random($status),
+        'status' => 'student',
         'code' =>  Str::random(4),
         'remember_token' => Str::random(10),
     ];
